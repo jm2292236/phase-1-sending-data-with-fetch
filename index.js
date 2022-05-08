0dae0fcb63010d9421b1037995b0b6f39393d0c9
Jaime Montes de Oca <jaime0422@live.com>
2022-05-08 12:00:22 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const hostURL = "http://localhost:3000/users";
+    const userData = {name: userName, email: userEmail};
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(userData)
+    };
+    
+    return fetch(hostURL, configurationObj)
+        .then (response => response.json())
+        .then (object => {
+            const div = document.querySelector("div");
+            const p = document.createElement("p");
+            p.innerHTML = `ID for the new User: ${object.id}`;
+            div.appendChild(p);
+        })
+        .catch (error => {
+            const div = document.querySelector("div");
+            const p = document.createElement("p");
+            p.innerHTML = `Unauthorized Access (Server error: ${error.message})`;
+            div.appendChild(p);
+        })
+}
+
+submitData("Johny", "roadWarrior@aol.com");
